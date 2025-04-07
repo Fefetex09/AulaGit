19862a2ef22f96be41784ead0368453538c4f0f6
Fellipe <fellipet.30@gmail.com>
2025-04-07 15:21:17 -0300

    Incluído ordenação da lista

---
+++ git.js
@@ -0,0 +1,4 @@
+let comidas = ["Lasanha", 'pizza', 'sushi', 'hamburguer'];
+console.log(`Lista de todas as comidas: ${comidas}`);
+console.log(`primeira comida: ${comidas[0]}`); 
+comidas.sort(`Comidas ordenadas: ${comidas}`); 
